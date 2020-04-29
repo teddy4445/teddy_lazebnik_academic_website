ca211f36854cec9f890f4ff041ca2830fe2e743d
teddy@dnc-algo.com <lazebnik.teddy@gmail.com>
2020-04-29 15:53:39 +0300

    start adding data

---
+++ medical_flow_graph_creator/vassal_calculator.js
@@ -0,0 +1,21 @@
+class VassalCalculator
+{
+	constructor()
+	{
+		
+	}
+	
+	static tranform(diameter, distensibility)
+	{
+		
+	}
+}
+
+
+
+/*
+	Python code of the data of points in both tables
+    data = [[1.41, 2.95, 0.15], [0.51, 2.45, 0.15], [0.51, 1.40, 0.11], [0.47, 1.33, 0.09]]
+    x = [[item[0], item[1]] for item in data]
+    y = [item[2] for item in data]
+*/
