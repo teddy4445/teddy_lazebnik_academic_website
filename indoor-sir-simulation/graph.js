ddc7648e89dad6347b65d9e6a17d06645d3b3c6b
teddy <lazebnik.teddy@gmail.com>
2020-12-26 16:43:39 +0200

    small view improvments

--- indoor-sir-simulation/graph.js
+++ indoor-sir-simulation/graph.js
@@ -151,16 +151,29 @@ class Node
 		// visualization members
 		this.polyPoints = polyPoints;
 		
-		var centerX = 0;
-		var centerY = 0;		
-		for (var pointIndex = 0; pointIndex < this.polyPoints.length; pointIndex++)
+		var first = polyPoints[0];
+		var last = polyPoints[polyPoints.length-1];
+		if (first.x != last.x || first.y != last.y) 
 		{
-			centerX += this.polyPoints[pointIndex].x;
-			centerY += this.polyPoints[pointIndex].y;
+			polyPoints.push(first);
 		}
-		centerX /= this.polyPoints.length;
-		centerY /= this.polyPoints.length;
-		this.center = createVector(centerX, centerY); 
+		var twicearea = 0;
+		var x = 0;
+		var y = 0;
+		var p1;
+		var p2;
+		var f;
+		for (var i = 0, j = polyPoints.length - 1 ; i < polyPoints.length; j = i++) 
+		{
+			p1 = polyPoints[i];
+			p2 = polyPoints[j];
+			f = p1.x * p2.y - p2.x * p1.y;
+			twicearea += f;
+			x += ( p1.x + p2.x ) * f;
+			y += ( p1.y + p2.y ) * f;
+		}
+		f = twicearea * 3;
+		this.center = createVector(x/f, y/f); 
 	}
 	
 	print()
